2cd2c70c34f8c23c34a8774ddd4176575fcf9845
Norton M. B. Santos <norton@alunos.utfpr.edu.br>
2018-05-03 18:30:24 -0300

    Add image to Room

---
+++ db/migrate/20180503162956_add_image_to_rooms.rb
@@ -0,0 +1,5 @@
+class AddImageToRooms < ActiveRecord::Migration[5.0]
+  def change
+    add_column :rooms, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502190922) do
+ActiveRecord::Schema.define(version: 20180503163009) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180502190922) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "image"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
@@ -35,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180502190922) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
